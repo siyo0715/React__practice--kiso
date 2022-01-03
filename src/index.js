45f3751add106031c0fd8d51a686e994cf9daf60
siyo0322 <86521768+siyo0715@users.noreply.github.com>
2022-01-03 22:54:32 +0900

    [Add]HelloWorld [Add]HelloWorld

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  return <h1>HelloWorld</h1>;
 };
 
 ReactDom.render(<App />, document.querySelector("#root"));
