97b6e0a14b0cf88070d86cc73334af25f1eed6f1
siyo0322 <86521768+siyo0715@users.noreply.github.com>
2022-01-04 16:49:25 +0900

    [Add]objでstyle [Add]objでstyle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>HelloWorld</h1>
-      <p>お元気ですか？</p>
+      <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
