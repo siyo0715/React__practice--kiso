7618a236bfe068ac38035c437533401508ff97cb
siyo0322 <86521768+siyo0715@users.noreply.github.com>
2022-01-04 17:36:41 +0900

    [Add]style共通 [Add]style共通

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentLedyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>HelloWorld</h1>
       <ColofulMessage color="blue" message="お元気ですか？" />
-      <p style={contentLedyStyle}>元気です！</p>
+      <ColofulMessage color="pink" message="お元気です" />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
