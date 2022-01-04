ce4461eac847ac8555d753afb6b971b41bbddaf8
siyo0322 <86521768+siyo0715@users.noreply.github.com>
2022-01-04 17:32:26 +0900

    [fix]props.color [fix]props.color

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ColorfulMessage = (props) => {
   const contentStyle = {
-    color: "blue",
+    color: props.color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>お元気ですか？</p>;
+  return <p style={contentStyle}>{props.message}</p>;
 };
 
 export default ColorfulMessage;
